bde13da3d2f27509951a52b4b6f3e69c6b2ab79e
o5edaxi <88740706+o5edaxi@users.noreply.github.com>
2022-02-21 14:12:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# paloversion-gui
+
+### Details
+
+This script provides a graphical interface for the [paloversion](https://github.com/o5edaxi/paloversion) script running in batch mode.
+
+### Requirements
+
+* The device running the script must be connected to the same broadcast domain as the firewall's MGMT port.
+* The script is intended to upgrade firewalls with factory settings. To use a custom password, modify the **BACKUP_PASSWORD** variable inside paloversion.sh
+
+### Installation on Raspberri Pi + Touch Display
+
+The following steps will setup a Raspberry to run the script at boot and load the firmware from a USB stick. A single 3B+ model is enough to upgrade 20+ firewalls per batch fairly quickly.
+
+1. Install Raspbian: https://www.raspberrypi.org/documentation/computers/getting-started.html
+2. Update packages and then install **cURL**, **iproute2**, and **xmlstarlet**
+3. Install Kivy as explained in https://kivy.org/doc/stable/installation/installation-rpi.html:
+    
+    a. Essentially, install the dependencies listed here: https://kivy.org/doc/stable/installation/installation-rpi.html#install-source-rpi
+    
+    b. Then install Kivy: **pip3 install kivy[base,media]**
+    
+    c. Also, when using the official RPi touch display, add the following to the **[input]** section of **/home/pi/.kivi/config.ini** (remove the "mtdev" line if you notice difficulties navigating the touch menus in the script):
+    
+```
+mouse = mouse
+mtdev_%(name)s = probesysfs,provider=mtdev
+hid_%(name)s = probesysfs,provider=hidinput
+```
+
+5. Format the USB drive using FAT32. This will allow it to be read and written by the Raspberry as well as a Windows machine. The drive should have enough space to contain all the images needed for upgrades.
+6. Place all firmware files, .csv files, and App/Threat packages in the USB drive
+7. Place paloversion.sh and paloversion-gui.py in a folder on the Raspberry's SD card (for example /home/pi/Desktop) and ensure they are executable
+8. Ensure the USB drive automounts **as read-only** and consistently under the same folder each time it is inserted:
+
+    a. Disable pcmanfm automounting by opening a File Manager window, then selecting **>Edit>Preferences>Volume** and unchecking all boxes
+    
+    b. Install usbmount: **apt-get install usbmount**
+    
+    c. Add "ro" to the MOUNTOPTIONS in file **/etc/usbmount/usbmount.conf**
+    
+    d. Run **sudo systemctl edit systemd-udevd** and enter the following in the indicated empty space, then save, close, and reboot the Raspberry:
+    
+```
+[Service]
+PrivateMounts=no
+```
+
+8. (Optional) make your /boot partition read-only to decrease the chances that it might become corrupted when removing power to the Raspberry
+9. To make the GUI start fullscreen after boot ("kiosk" style), create file **palo.desktop** under **/home/pi/.config/autostart/** with the following contents (make sure you insert the correct python and script locations):
+
+```
+[Desktop Entry]
+Type=Application
+Name=PaloUpgrade
+Exec=/usr/bin/python3 /home/pi/Desktop/paloversion-gui.py
+```
+
+10. Adjust variables **FIRMWARE_PATH**, **SCRIPT_PATH**, **SCRIPT_ROOT** in paloversion-gui.py to match your folder structure
+11. (Optional) set up networking and SSH access for your Raspberry, or use mouse + keyboard to manage it directly
+12. Done! The script should now start up whenever the Raspberry boots. Simply connect the Raspberry to the MGMT port of a firewall, turn it on, and it will be automatically discovered.
+
+
+### License
+
+This project is licensed under the [MIT License](https://github.com/o5edaxi/paloversion-gui/blob/main/LICENSE).
---
+++ paloversion-gui.py
@@ -0,0 +1,334 @@
+#! /usr/bin/env python3
+
+import os
+import subprocess
+import sys
+import time
+import re
+import csv
+import shutil
+import logging
+
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.app import App
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+# Customize these if necessary
+########################
+FIRMWARE_PATH = "/media/usb0/"
+NETWORK_INTERFACE = "eth0"
+SCRIPT_PATH = "/home/pi/Desktop/paloversion.sh"
+SCRIPT_ROOT = "/home/pi/Desktop/"
+CSV_DELIM = ","
+########################
+
+TEXT1 = """Remove power to shutdown or restart the Raspberry at any time.
+
+1. Place the firmware files and the up to date .csv in the usb drive
+2. Connect the Raspberry to the firewall, or multiple firewalls using a switch
+3. Make sure the password is admin/admin or admin/Admin123
+4. Select the desired version and press START
+5. The jobs are yellow when in progress, red when failed, and green when completed
+6. Tap on the serial number of a firewall to view the detailed upgrade logs"""
+
+DESIRED_VERSION = "Pan-OS Selection"
+Window.fullscreen = 'auto'
+Window.show_cursor = False
+OPTIONS = []
+yellow = [1, 1, 0, 1]
+green = [0, 1, 0, 1]
+red = [1, 0, 0, 1]
+
+chosenserial = ""
+status_array = []
+
+regexcsv = re.compile(r'^\w+\.csv$')
+regexver = re.compile(r'^[0-9.a-zA-z]+$')
+regexlogs = re.compile(r'^(PaloVersionBatch|\d+|fe80[\w:]+)\.log$')
+
+logging.basicConfig(filename="/home/pi/app.logs", filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S', level=logging.INFO)
+
+
+def monitor_files(path):
+    matches = []
+    for root, _, files in os.walk(path):
+        for file in files:
+            if re.match(regexlogs, file) and file != "PaloVersionBatch.log":
+                try:
+                    with open(os.path.join(root, file), 'r') as f:
+                        contents = f.read()
+                        if contents != "":
+                            active_serial = [file, yellow]
+                            if "---FAILED---" in contents:
+                                active_serial[1] = red
+                            elif "---FINISHED---" in contents:
+                                active_serial[1] = green
+                            matches.append(active_serial)
+                except Exception:
+                    active_serial = [file, red]
+                    matches.append(active_serial)
+    return matches
+
+
+def tail_file(serial):
+    file_contents = ""
+    try:
+        if serial != "":
+            with open(os.path.join(SCRIPT_ROOT, serial), 'r') as f:
+                file_contents = f.read()
+    except Exception as e:
+        if serial == "PaloVersionBatch.log":
+            logging.error('Error reading log file for serial {} with exception {}.\nCheck that '
+                          'firewalls are reachable and MAC address is in filter.'.format(serial, e))
+            file_contents = ('Error reading log file for serial {} with exception {}.\nCheck that '
+                             'firewalls are reachable and MAC address is in filter.'.format(serial,
+                                                                                            e))
+        else:
+            logging.error('Error reading log file for serial {} with exception {}'.format(serial,
+                                                                                          e))
+            file_contents = 'Error reading log file for serial {} with exception {}'.format(serial,
+                                                                                            e)
+    return file_contents
+
+
+def get_versions(path):
+    matches = []
+    for root, _, files in os.walk(path):
+        for file in files:
+            if re.match(regexcsv, file):
+                try:
+                    shutil.copyfile(os.path.join(root, file), os.path.join(SCRIPT_ROOT, file))
+                except Exception as e:
+                    logging.error("Failed to copy file {} from usb key, {}".format(file, e))
+                try:
+                    if file == "content.csv":
+                        continue
+                    with open(os.path.join(root, file), 'r') as f:
+                        csvobj = csv.reader(f, delimiter=CSV_SEP)
+                        rows = list(csvobj)
+                        for row in rows:
+                            if row[1] not in matches and re.match(regexver, row[1]):
+                                matches.append(row[1])
+                except Exception as e:
+                    logging.error("Error opening file {}{}, {}".format(root, file, e))
+    matches.sort(reverse=True)
+    return matches
+
+
+def cleanup(path):
+    for root, _, files in os.walk(path):
+        for file in files:
+            if re.match(regexlogs, file):
+                try:
+                    os.remove(os.path.join(root, file))
+                except Exception:
+                    logging.error("Failed to remove old log {}".format(file))
+                    pass
+
+
+class Panel1(Screen):
+    def __init__(self, **kwargs):
+        super(Panel1, self).__init__(**kwargs)
+        box = BoxLayout(orientation='vertical')
+        label = Label(text=TEXT1)
+        mac_button = ToggleButton(text='Exclude non-PA MAC addresses', font_size=25,
+                                  size_hint_y=0.2, state='down')
+        button = Button(text=DESIRED_VERSION, font_size=30, size_hint_y=0.35,
+                        on_release=self.list)
+        box.add_widget(label)
+        box.add_widget(button)
+        box.add_widget(mac_button)
+        enterbutton = Button(text='START', font_size=30, size_hint_y=0.35, on_release=self.close)
+        box.add_widget(enterbutton)
+        self.add_widget(box)
+
+    def list(self, button):
+        self.parent.current = 'Version'
+
+    def close(self, bar):
+        if DESIRED_VERSION != "Pan-OS Selection":
+            if self.children[0].children[1].state == "down":
+                # MAC OUI filter yes or no
+                command = '{} -f -z "" "" "" "{}" "{}" "{}"'.format(SCRIPT_PATH, FIRMWARE_PATH,
+                                                                    DESIRED_VERSION,
+                                                                    NETWORK_INTERFACE)
+            else:
+                command = '{} -f -z -m "" "" "" "{}" "{}" "{}"'.format(SCRIPT_PATH, FIRMWARE_PATH,
+                                                                       DESIRED_VERSION,
+                                                                       NETWORK_INTERFACE)
+            subprocess.Popen(command, shell=True, stdin=None, stdout=open(os.devnull, 'wb'),
+                             stderr=open(os.devnull, 'wb'), cwd=SCRIPT_ROOT)
+            time.sleep(1)
+            self.parent.current = 'General'
+            
+    def on_pre_enter(self):
+        self.children[0].children[2].text=DESIRED_VERSION
+
+
+class Panel2(Screen):
+    def __init__(self, **kwargs):
+        global status_array
+        super(Panel2, self).__init__(**kwargs)
+        Clock.schedule_interval(self.refresher, 10)
+        box = GridLayout(cols=1, row_force_default=True, row_default_height=40, spacing=[1, 1],
+                         size_hint=(1, None), padding=[0, 0, 100, 0])
+        box.bind(minimum_height=box.setter('height'))
+        button = Button(text="Tap a serial number to view the detailed upgrade logs", font_size=25,
+                        background_normal="", background_color=yellow, color=[0, 0, 0, 1])
+        box.add_widget(button)
+        button = Button(text="PaloVersionBatch.log", font_size=30, background_normal="",
+                        background_color=yellow, color=[0, 0, 0, 1], on_release=self.drilldown)
+        box.add_widget(button)
+        status_array = monitor_files(SCRIPT_ROOT)
+        for serial in status_array:
+            button = Button(text=str(serial[0]), font_size=30, background_normal="",
+                            background_color=serial[1], color=[0, 0, 0, 1],
+                            on_release=self.drilldown)
+            box.add_widget(button)
+        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height),
+                          scroll_type=['content'])
+        root.add_widget(box)
+        self.add_widget(root)
+
+    def refresher(self, bar):
+        global status_array
+        status_array = monitor_files(SCRIPT_ROOT)
+        self.children[0].children[0].clear_widgets()
+        button = Button(text="Tap a serial number to view the detailed upgrade logs", font_size=25,
+                        background_normal="", background_color=yellow, color=[0, 0, 0, 1])
+        self.children[0].children[0].add_widget(button)
+        if red in [item[1] for item in status_array]:
+            maincolor = red
+            maintext = "Upgrades finished with errors"
+        elif yellow in [item[1] for item in status_array]:
+            maincolor = yellow
+        elif status_array == []:
+            maincolor = yellow
+        else:
+            maincolor = green
+            maintext = "Upgrades finished without errors"
+        button = Button(text="PaloVersionBatch.log", font_size=30, background_normal="",
+                        background_color=maincolor, color=[0, 0, 0, 1], on_release=self.drilldown)
+        self.children[0].children[0].add_widget(button)
+        if maincolor != yellow:
+            self.children[0].children[0].add_widget(Button(text=maintext, font_size=25,
+                                                           background_normal="",
+                                                           background_color=maincolor,
+                                                           color=[0, 0, 0, 1]))
+        for serial in status_array:
+            button = Button(text=str(serial[0]), font_size=30, background_normal="",
+                            background_color=serial[1], color=[0, 0, 0, 1],
+                            on_release=self.drilldown)
+            self.children[0].children[0].add_widget(button)
+
+    def drilldown(self, button):
+        global chosenserial
+        chosenserial = button.text
+        self.parent.current = 'Details'
+
+
+class Panel3(Screen):
+    def __init__(self, **kwargs):
+        super(Panel3, self).__init__(**kwargs)
+        Clock.schedule_interval(self.refresher, 5)
+        label = Label(text=tail_file(chosenserial), size_hint=(None, None))
+        label.bind(texture_size=lambda *x: (label.setter('height')(label, label.texture_size[1]),
+                                            label.setter('width')(label, label.texture_size[0])))
+        scroll = ScrollView(size_hint=(None, None), size=(Window.width, Window.height),
+                            pos_hint={'top': 1, 'right': 1})
+        scroll.add_widget(label)
+        btn = Button(text='Back', font_size=30, background_normal="", background_color=red,
+                     color=[0, 0, 0, 1], on_release=self.close, size_hint=(None, None),
+                     size=(75, 50), pos_hint={'top': 1, 'right': 1})
+        grid = RelativeLayout()
+        grid.add_widget(scroll)
+        grid.add_widget(btn)
+        self.add_widget(grid)
+
+    def refresher(self, bar):
+        self.children[0].children[1].children[0].text = tail_file(chosenserial)
+
+    def close(self, bar):
+        global chosenserial
+        chosenserial = ""
+        self.parent.current = 'General'
+
+    def on_pre_enter(self):
+        self.refresher(self)
+
+
+class Panel4(Screen):
+    def __init__(self, **kwargs):
+        super(Panel4, self).__init__(**kwargs)
+        scroll = ScrollView(size_hint=(1, None), size=(Window.width, Window.height),
+                            scroll_type=['content'])
+        box = GridLayout(cols=1, row_default_height=40, spacing=[1, 1],
+                         size_hint=(1, None), padding=[0, 0, 100, 0])
+        box.bind(minimum_height=box.setter('height'))
+        for option in OPTIONS:
+            btn = Button(text=option, size_hint_y=None, height=44)
+            btn.text = option
+            btn.bind(on_release=lambda btn: self.close(btn.text))
+            box.add_widget(btn)
+        scroll.add_widget(box)
+        anchor = AnchorLayout(anchor_x='right')
+        helptext = Label(text="[color=ffffff]Scroll[/color]", size_hint_x=None, markup=True)
+        anchor.add_widget(helptext)
+        anchor.add_widget(scroll)
+        self.add_widget(anchor)
+
+    def close(self, text):
+        global DESIRED_VERSION
+        DESIRED_VERSION = text
+        self.parent.current = 'Intro'
+
+
+class Manager(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(Panel1(name='Intro'))
+        sm.add_widget(Panel2(name='General'))
+        sm.add_widget(Panel3(name='Details'))
+        sm.add_widget(Panel4(name='Version'))
+        return sm
+
+
+class PanelError(App):
+    def build(self):
+        anchor = AnchorLayout()
+        anchor.add_widget(Label(text="USB not inserted. Please insert and restart.", font_size=40))
+        return anchor
+
+
+time_start = time.time()
+
+while True:
+    try:
+        if len(os.listdir(FIRMWARE_PATH)) != 0:
+            break
+    except Exception:
+        logging.critical('USB not inserted')
+        PanelError().run()
+        sys.exit(1)
+    if time.time() - time_start > 10:
+        logging.critical('USB not inserted')
+        PanelError().run()
+        sys.exit(1)
+
+cleanup(SCRIPT_ROOT)
+OPTIONS = get_versions(FIRMWARE_PATH)
+
+Manager().run()
